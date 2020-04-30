b8964dc92a17686c808a4d7820ef3785171f896a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-30 14:08:34 +0700

    Handling when options menu is clicked

--- app/src/main/java/com/erikriosetiawan/tokohreogponorogo/MainActivity.java
+++ app/src/main/java/com/erikriosetiawan/tokohreogponorogo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.erikriosetiawan.tokohreogponorogo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -36,5 +38,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item_about_me) {
+            Intent intent = new Intent(this, AboutMeActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
